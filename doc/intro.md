372cc43c9f623dd438438c614506dbd91d1df2fe
chanshunli <chanshunli@gmail.com>
2017-05-04 23:13:44 +0800

    init

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to hanlping
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/hanlping/core_test.clj
@@ -0,0 +1,7 @@
+(ns hanlping.core-test
+  (:require [clojure.test :refer :all]
+            [hanlping.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
