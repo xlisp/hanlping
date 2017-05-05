69650496b262f01405a60a9858515c6cc86afbb4
chanshunli <chanshunli@gmail.com>
2017-05-05 08:40:13 +0800

    初始化 new CRFSegment

--- src/hanlping/core.clj
+++ src/hanlping/core.clj
@@ -1,6 +1,12 @@
-(ns hanlping.core)
+(ns hanlping.core
+  (:import [com.hankcs.hanlp.seg.CRF CRFSegment]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;; 从官方下载data.zip,然后解压到当前项目目录下:
+;; 加载"./data/model/segment/CRFSegmentModel.txt",就是加载了CRFSegmentModel.txt.bin,因为会自动加bin的后缀
+(defn init-crfs-segment
+  [path]
+  (CRFSegment. path))
+
+;; (memoized-segment "./data/model/segment/CRFSegmentModel.txt")
+(def memoized-segment (memoize init-crfs-segment))
+
