c4b5226d4b8df5596be3659a14a7d53b4ff2523e
chanshunli <chanshunli@gmail.com>
2017-05-15 15:31:00 +0800

    增加类型限制,提高Clojure运行速度

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject hanlping "0.1.0-SNAPSHOT"
+(defproject hanlping "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [com.hankcs/hanlp "portable-1.3.2"]]
-  :repositories {"aliyun" "http://maven.aliyun.com/nexus/content/groups/public"} )
+  :repositories {"aliyun" "http://maven.aliyun.com/nexus/content/groups/public"}
+  :main hanlping.core)
--- src/hanlping/core.clj
+++ src/hanlping/core.clj
@@ -1,24 +1,43 @@
 (ns hanlping.core
-  (:import [com.hankcs.hanlp.seg.CRF CRFSegment]))
+  (:import
+   [java.util.List]
+   [com.hankcs.hanlp.seg Segment]
+   [com.hankcs.hanlp.seg.CRF CRFSegment]
+   [com.hankcs.hanlp.seg.common Term]))
+
+(set! *warn-on-reflection* true)
 
 (def crfs-segment-path "./data/model/segment/CRFSegmentModel.txt")
 
 ;; 从官方下载data.zip,然后解压到当前项目目录下:
 ;; 加载"./data/model/segment/CRFSegmentModel.txt",就是加载了CRFSegmentModel.txt.bin,因为会自动加bin的后缀
 (defn init-crfs-segment
-  [path]
+  [^String path]
   (CRFSegment. path))
 
 ;; 只要调用一次就会保存到内存: (memoized-segment "./data/model/segment/CRFSegmentModel.txt")
 (def memoized-segment (memoize init-crfs-segment))
 
-;; (to-seg-list "你看过穆赫兰道吗" "江西鄱阳湖干枯") ;;=> (("你" "看过" "穆赫兰道" "吗") ("江西" "鄱阳湖" "干枯"))
+;; (to-seg-list "你看过穆赫兰道吗" "江西鄱阳湖干枯") ;;=> [[[r 你] [v 看过] [nz 穆赫兰道] [y 吗]] [[ns 江西] [ns 鄱阳湖] [a 干枯]]]
+;; n* 就是名称,按需要过滤出来
 (defn to-seg-list
   [& str-list]
-  (let [segment (memoized-segment crfs-segment-path)
+  (let [^Segment segment (memoized-segment crfs-segment-path)
         _ (.enablePartOfSpeechTagging segment true)
-        seg-fn (fn [st] (map #(.word %) (.seg segment st)))
+        seg-fn (fn [^String st]
+                 (mapv
+                  (juxt #(.toString (.nature ^Term %)) #(.word ^Term %))
+                  (.seg
+                   segment
+                   st)
+                  ))
         ]
-    (map seg-fn str-list)
+    (mapv seg-fn str-list)
     )
+  )
+
+;; Run 一些测试的例子
+(defn -main
+  [& args]
+  (println (to-seg-list "你看过穆赫兰道吗" "江西鄱阳湖干枯的"))
   )
